15c3016940f47f0599643f6f632828dd07c87721
SANDHYAAMMU11 <147844282+SANDHYAAMMU11@users.noreply.github.com>
2023-10-15 15:02:13 +0530

    Update urls.py

--- todoproject/todoapp/urls.py
+++ todoproject/todoapp/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.add, name='add'),
-    path('/delete<int:taskid>/', views.delete,name='delete'),
-    path('/update<int:id>/', views.update, name='update'),
+    path('delete<int:taskid>/', views.delete,name='delete'),
+    path('update<int:id>/', views.update, name='update'),
     path ('cbvhome/',views.Tasklistview.as_view(),name='cbvhome')
 ]
